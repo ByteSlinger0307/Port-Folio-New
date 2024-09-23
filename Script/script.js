9ec9c71f87c1e28d209a056a55800dee03bebca3
ByteSlinger0307 <dubeykrish0307@gmail.com>
2024-09-24 03:25:20 +0530

    Created preloader

--- Script/script.js
+++ Script/script.js
@@ -3,6 +3,35 @@ const scroll = new LocomotiveScroll({
     smooth: true
 });
 
+let intro = document.querySelector('.intro');
+let logo = document.querySelector('.logo');
+let logospan = document.querySelectorAll('.logo-parts');
+
+window.addEventListener('DOMContentLoaded', () => {
+    setTimeout(() => {
+       logospan.forEach((span, index) =>{
+        setTimeout(() => {
+           span.classList.add('active');
+
+        }, (index + 1) * 100);
+       }); 
+
+       setTimeout(() => {
+        logospan.forEach((span, index) =>{
+            setTimeout(() => {
+                span.classList.remove('active');
+                span.classList.add('fade'); 
+            }, (span + 1) * 50);
+        });
+       }, 2000);
+
+       setTimeout(() => {
+        intro.style.top = '-100vh';
+       }, 2300);
+
+    });
+});
+
 function firstPageAnimation(){
     var tl = gsap.timeline();
 
